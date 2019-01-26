ba41e267e5cc1b83215f82e4507c76b8fd381d69
小键子 <chengjunjian@126.com>
2019-01-26 15:52:48 +0800

    update

--- Linked-List/src/LinkedList.java
+++ Linked-List/src/LinkedList.java
@@ -5,7 +5,7 @@ public class LinkedList<E> {
         public Node next;
 
         public Node(E e,Node next){
-            this.e = e;
+            this.e=e;
             this.next=next;
         }
         public Node(E e){
@@ -17,52 +17,87 @@ public class LinkedList<E> {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "e=" + e +
-                    ", next=" + next +
-                    '}';
+            return e.toString();
         }
     }
-    private Node head;
+
+    private Node dummyHead;
     private int size;
 
-    //获取链表个数
-    public int getSize(){
-        return size;
+    public LinkedList(){
+        dummyHead=new Node();
+        size=0;
     }
-    //链表是否为空
-    public boolean isEmpty(){
-        return size == 0;
-    }
-    //在表头添加元素
-    public void addFirst(E e){
-        Node node = new Node(e);
-        node.next=head;
-        head = node;
-        size++;
-    }
-
     public void add(int index,E e){
         if(index<0 || index>size){
-            throw new IllegalArgumentException("index参数异常");
+            throw new IllegalArgumentException("index argument exception");
         }
-        if(index==0){
-            addFirst(e);
-        }else{
-            Node prev = head;
-            for(int i=0;i<index-1;i++){
-                prev = prev.next;
-            }
-            Node node = new Node(e);
-            node.next = prev.next;
-            prev.next = node;
-            size++;
+        Node prev = dummyHead;
+        for(int i=0;i<index;i++){
+            prev = prev.next;
         }
+        Node node = new Node(e);
+        node.next=prev.next;
+        prev.next=node;
+        size++;
     }
-
-    //在表尾添加元素
+    public void addFirst(E e){
+        add(0,e);
+        size++;
+    }
     public void addLast(E e){
         add(size,e);
     }
+    public E get(int index){
+        if(index<0 || index>=size){
+            throw new IllegalArgumentException("index argument exception");
+        }
+        Node cur = dummyHead;
+        for(int i=0;i<index;i++){
+            cur = dummyHead.next;
+        }
+        return cur.e;
+    }
+    // 获得链表的第一个元素
+    public E getFirst(){
+        return get(0);
+    }
 
+    // 获得链表的最后一个元素
+    public E getLast(){
+        return get(size - 1);
+    }
+    //修改链表中的元素
+    public void set(int index,E e){
+        if(index<0 || index>=size){
+            throw new IllegalArgumentException("index argument exception");
+        }
+        Node cur = dummyHead;
+        for(int i=0;i<index;i++){
+            cur = dummyHead.next;
+        }
+        cur.e=e;
+    }
+    //查找链表中是否包含这个元素
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while(cur !=null){
+            if(cur.e.equals(e)){
+                return true;
+            }
+            cur=cur.next;
+        }
+        return false;
+    }
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while(cur!=null){
+            res.append(cur+"->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
 }
--- Linked-List/src/Main.java
+++ Linked-List/src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
-    public static void main(String args[]) {
-        LinkedList list = new LinkedList();
-        list.addFirst("a");
-        list.add(1,"2");
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0 ; i < 5 ; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
     }
 }
